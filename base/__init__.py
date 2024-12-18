24fdc39ac9060bab225c259c9380c639d78dee92
e23007 <e23007@std.it-college.ac.jp>
2024-12-18 07:05:02 +0000

    radix trans

---
+++ base/__init__.py
@@ -0,0 +1,2 @@
+from .main import base  # 同じフォルダのmain.pyにあるfunc1をimport
+__all__ = ['base']  # func1をpackageから呼べるようにする
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, base
+
+setup(
+    name='baes',  # パッケージ名（pip listで表示される）
+    version="0.0.1",  # バージョン
+    description="radix transformation",  # 説明
+    author='e23007',  # 作者名
+    packages=base(),  # 使うモジュール一覧を指定する
+    license=''  # ライセンス
+)
